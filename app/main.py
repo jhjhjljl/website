4a2790b5ea62ca8c89e2878db13b969e7cf86ca6
Justin Lee <justinlsoftware@gmail.com>
2026-04-08 12:39:07 -0400

    Add dummy templates.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from routers import index
 
 
-app = FastAPI()
+app = FastAPI(openapi_url=None)
 
 
 app.mount("/static", StaticFiles(directory="static"))
